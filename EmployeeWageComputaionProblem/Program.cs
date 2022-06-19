7a4b67cd7d8072d8f6ed178dc314a054fd29436d
Prateek Pai <paiprateek123@gmail.com>
2022-06-19 21:15:21 +0530

    Welcome to Employee Wage Computation Problem

---
+++ EmployeeWageComputaionProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageComputaionProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWage.CheckEmpWage();
+        }
+    }
+}
